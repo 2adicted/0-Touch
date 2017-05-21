fbc71afa07fffd29ba0efcc290487f13c69397a6
Deyan Nenov <Deyan Nenov>
2017-05-21 23:09:41 +0100

    Initia

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Proto = Revit.Elements;
+
+namespace DynamoNodes
+{
+    public class ImportedInstance
+    {
+        internal ImportedInstance()
+        {
+
+        }
+        public static List<bool> IsLinked([DefaultArgument("{}")] IList elements)
+        {        
+            List<bool> value = new List<bool>();
+
+            foreach(var ins in elements)
+            {
+                var ce = (Proto.Element) ins;
+                Element element = ce.InternalElement;
+                ImportInstance instance = element as ImportInstance;
+                
+                if (instance == null)
+                {
+                    TaskDialog.Show("Error", "Expecting Impored Instance");
+                    return value;
+                }
+                else
+                {
+                    value.Add(instance.IsLinked);
+                }
+            }
+
+            return value;
+        }
+    }
+}
